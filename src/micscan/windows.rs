06c7e628a6b59cf58e2ca4ffbe11b7952c79e33f
MPE <mpe@systerel.fr>
2023-03-27 08:25:58 +0200

    Set status depending of mic usage (windows)

--- src/micscan/windows.rs
+++ src/micscan/windows.rs
@@ -1,9 +1,41 @@
 use anyhow::Result;
-//use tracing::debug;
+use tracing::debug;
+use winreg::enums::*;
+use winreg::RegKey;
 
 /// Return the list of application name using the default microphone,
-/// either via pulseaudio or alsa depending upon compilation option.
-/// TODO for windows
+/// by reading the database register.
 pub fn processes_owning_mic() -> Result<Vec<String>> {
-    return Ok(vec![]);
+    let mut res = Vec::new();
+    let hklm = RegKey::predef(HKEY_CURRENT_USER);
+
+    //Retrieve the "parent" key : under it, all application that can used the micro.
+    let mic_info_path = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\CapabilityAccessManager\\ConsentStore\\microphone\\NonPackaged";
+    let cur_ver = hklm.open_subkey(mic_info_path)?;
+
+    //Iterate on "child" keys
+    for child_keys in cur_ver.enum_keys().map(|x| x.unwrap())
+    {
+        let process = cur_ver.open_subkey(child_keys.clone())?;
+        
+        //Iterate on key's "values". Keys name are the absolute path of the application with "/" replace by "#".
+        // Example : C:#Program Files (x86)#ZoomRooms#bin#ZoomRooms.exe.
+        for (name, value) in process.enum_values().map(|x| x.unwrap()) {
+            
+            //Trigger on "LastUsedTimeStop" value : if equal to "0" (string), micro is currently in used by concerned application.
+            if name == "LastUsedTimeStop" && value.to_string() == "0" {
+                let process_path = child_keys.to_string();
+                
+				//Retrieve only application name (with extension)
+                let process_path_splitted: Vec<&str> = process_path.split("#").collect();
+                if let Some(process_name) = process_path_splitted.last() { 
+                    res.push(process_name.to_string());
+                } 
+                
+            }
+        } 
+    }
+	
+    debug!("Process owning mic : {:?}", res);
+    Ok(res)
 }
